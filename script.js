31b2f24b3325a1dee91416205b15aa2bdea1a225
Tejshwar007 <84656429+Tejshwar007@users.noreply.github.com>
2021-05-24 16:39:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+ 
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+{   
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var interest =(p*r*y)/100;
+     document.getElementById("result").innerHTML="If you deposit" +p+ "at a rate of" +r+ " for the time of" +y+ "years, you will receive " +interest+ "interest";
+}  
+
+     function updateRate()
+     {
+            
+            var rateval= document.getElementById("rate").value;
+            document.getElementById("rate_val").innerText=rateval;
+
+         }
+        
+
+
+
+
         
